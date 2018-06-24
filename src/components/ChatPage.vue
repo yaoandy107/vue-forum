ec60640dd9cc19f01e0717597778604fd4987ce1
yaoandy107 <yaoandy107@gmail.com>
2018-06-24 16:57:16 +0800

    刪除無用的data

--- src/components/ChatPage.vue
+++ src/components/ChatPage.vue
@@ -9,24 +9,6 @@
 <script>
 export default {
   data: () => ({
-    title: '論壇',
-    dialog: false,
-    drawer: null,
-    items: [
-      { icon: 'contacts', text: '好友' },
-      { icon: 'history', text: '對話紀錄' },
-      {
-        icon: 'keyboard_arrow_up',
-        'icon-alt': 'keyboard_arrow_down',
-        text: '論壇版',
-        model: false,
-        children: [{ text: '學術' }, { text: '八卦' }, { text: '西施' }]
-      },
-      { icon: 'settings', text: '設定' },
-      { icon: 'chat_bubble', text: '回饋' },
-      { icon: 'help', text: '幫助' }
-      // { icon: 'phonelink', text: 'App 下載' },
-    ]
   }),
   props: {
     source: String
