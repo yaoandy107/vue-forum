68009a72d53372b23163111ef80e73b3198b61d6
yaoandy107 <yaoandy107@gmail.com>
2018-06-27 10:02:40 +0800

    新增聊天功能

---
+++ src/components/Chat.vue
@@ -0,0 +1,193 @@
+<template>
+    <v-container d-block fluid fill-height white>
+      <v-layout class="chat-body">
+          <!-- Main -->
+          <template v-for="item in messages">
+              <!-- other people -->
+              <template v-if="item.userId != globalObject.userId">
+                  <div class="message">
+                      <img src="https://vuetifyjs.com/static/doc-images/john.jpg" class="message__user" draggable="false">
+                      <div class="message__content">
+                          <div class="message__name">{{item.userName}}</div>
+                          <div v-if="item.type == 'text'" class="message__message">
+                            <div class="message__text">{{item.message}}</div>
+                          </div>
+                          <div v-if="item.type == 'image'" class="message__image"><img :src="item.message"></div>
+                      </div>
+                      <div class="message__time">{{item.timeStamp}}</div>
+                  </div>
+              </template>
+              <!-- 區塊：self -->
+              <template v-if="item.userId == globalObject.userId">
+                  <div class="message message--self">
+                      <div class="message__time">{{item.timeStamp}}</div>
+                      <div class="message__content">
+                          <div v-if="item.type == 'text'" class="message__message">
+                            <div class="message__text">{{item.message}}</div>
+                          </div>
+                          <div v-if="item.type == 'image'" class="message__image"><img :src="item.message"></div>
+                      </div>
+                  </div>
+              </template>
+          </template>
+      </v-layout>
+      <div class="chat-input">
+        <textarea id="js-message" class="chat-input__textarea" :class="{ disable: !userName }" @keydown.enter="sendMessage($event)"></textarea>
+      </div>
+    </v-container>
+</template>
+
+<script>
+import FirebaseHelper from '@/helpers/FirebaseHelper'
+
+export default {
+  watch: {
+    'globalObject.userId': function () {
+      const vm = this
+      if (!vm.globalObject.userId) {
+        vm.messages = []
+      }
+    }
+  },
+  created: function () {
+    this.setChat()
+  },
+  methods: {
+    setChat () {
+      const vm = this
+      const userId = vm.globalObject.userId
+      FirebaseHelper.getChat(userId, vm.friendId)
+        .onSnapshot((doc) => {
+          vm.chatId = doc.id
+          vm.messages = doc.data().messages || []
+        })
+    },
+    sendMessage (event) {
+      const vm = this
+      const message = document.querySelector('#js-message')
+      const userId = vm.globalObject.userId
+      const friendId = vm.friendId
+      // 如果是按住shift則不傳送訊息(多行輸入)
+      if (event.shiftKey) {
+        return false
+      }
+      // 如果輸入是空則不傳送訊息
+      if (message.value.length <= 1 && message.value.trim() === '') {
+        // 避免enter產生的空白換行
+        event.preventDefault()
+        return false
+      }
+      vm.messages.push({
+        userName: vm.userName,
+        userId: vm.globalObject.userId,
+        type: 'text',
+        message: message.value
+      })
+      FirebaseHelper.sendMessage(userId, friendId, vm.messages)
+      // 清空輸入欄位並避免enter產生的空白換行
+      message.value = ''
+      event.preventDefault()
+    }
+  },
+  data: () => ({
+    messages: [],
+    chatId: '',
+  }),
+  props: {
+    friendId: String
+  }
+}
+</script>
+
+<style>
+.chat-body {
+  display: block;
+  padding: 10px 0px;
+  background-color: #fff;
+  height: 80% !important;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+.message {
+  padding: 5px 10px;
+  position: relative;
+}
+.message__user {
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  vertical-align: top;
+  display: inline-block;
+  cursor: pointer;
+}
+.message__content {
+  max-width: 70%;
+  display: inline-block;
+}
+.message__name {
+  margin: 5px 0px 5px 5px;
+  font-size: 13px;
+  color: #727C8A;
+  vertical-align: top;
+  cursor: pointer;
+}
+.message__message {
+  margin: 5px 0px 5px 5px;
+  font-size: 12px;
+  color: #35393D;
+  letter-spacing: 0.6px;
+  background-color: #E3E8EB;
+  border-radius: 12px;
+  line-height: 1.5;
+  text-align: left;
+  word-break: break-all;
+  white-space: pre-line;
+}
+.message__text {
+  padding: 8px 10px 9px 11px;
+  max-height: 300px;
+  overflow: hidden;
+}
+
+.message__image {
+  margin: 5px 25px 5px 5px;
+}
+.message__time {
+  transform: scale(0.7);
+  color: #ACB0B8;
+  vertical-align: bottom;
+  margin: 0px 0px 5px -12px;
+  display: inline-block;
+}
+.message--self {
+  text-align: right;
+}
+.message--self .message__message {
+  background-color: #AFF47E;
+}
+.message--self .message__time {
+  margin: 0px -16px 5px 0px;
+}
+.chat-input {
+  height: 20%;
+  border: 1px gray solid;
+  padding: 10px 10px 5px 10px;
+}
+.chat-input__textarea {
+  width: 100%;
+  height: 100%;
+  border: none;
+  resize: none;
+  outline: none;
+}
+@media screen and (max-width: 425px) {
+  .message__content {
+    max-width: 60%;
+  }
+}
+@media screen and (max-width: 385px) {
+  .message__content {
+    max-width: 50%;
+  }
+}
+</style>
